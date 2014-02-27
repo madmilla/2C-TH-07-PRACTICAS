70f8a05a89215c14cd7a198cc3e195ab1ecc7574
bryanbaan <bryan.baan@student.hu.nl>
2014-02-27 20:35:45 +0100

    PixelDifferenceChecker is gefixt

--- THO7 Week 3/Efficientiecheck median filter en ruis remover/THO7AlgorithmTimer/PixelDifferenceChecker.cs
+++ THO7 Week 3/Efficientiecheck median filter en ruis remover/THO7AlgorithmTimer/PixelDifferenceChecker.cs
@@ -29,13 +29,22 @@ namespace THO7AlgorithmTimerApplication
 
         public String CheckDifferences(System.Drawing.Bitmap inputImage, System.Drawing.Bitmap noisedImage, System.Drawing.Bitmap outputImage)
         {
-            //These doubles represent the amount of ~white and ~black pixels in the new and old picture
-            double inWhite = 0;
-            double inBlack = 0;
-            double noiseWhite = 0;
-            double noiseBlack = 0;
-            double outWhite = 0;
-            double outBlack = 0;
+            //These doubles represent the amount of ~white and ~black pixels in the input, noised and output picture
+
+            //The first two doubles represent the amount of white and black pixels in the input picture (loaded from a directory without
+            //being filtered or added noise)
+            double inputImageWhitePixels = 0;
+            double inputImageBlackPixels = 0;
+
+            //The second two doubles represent the amount of white and black pixels in he noised picture (the input picture with added noise
+            //which cannot be seen in the GUI if you also choose to filter it)
+            double noisedImageWhitePixels = 0;
+            double noisedImageBlackPixels = 0;
+
+            //The third set of doubles represent the amount of white and black pixels in the output picture (this can be a noised image if no
+            //filter was chosen, but it can also be a noised picture which has been filtered if a filter was chosen)
+            double outputImageWhitePixels = 0;
+            double outputImageBlackPixels = 0;
 
             //These for loops will be used to run through the picture and check wether a pixel is black or white.
             //If a pixel is black or white, a counter will be incremented depending on what picture the algorithm found the pixel on.
@@ -45,42 +54,43 @@ namespace THO7AlgorithmTimerApplication
                 {
                     if (inputImage.GetPixel(x, y).B >= 240 && inputImage.GetPixel(x, y).G >= 240 && inputImage.GetPixel(x, y).R >= 240)
                     {
-                        inWhite++; 
-                    } 
+                        inputImageWhitePixels++;
+                    }
                     else if (inputImage.GetPixel(x, y).B <= 15 && inputImage.GetPixel(x, y).G <= 15 && inputImage.GetPixel(x, y).R <= 15)
                     {
-                        inBlack++;
+                        inputImageBlackPixels++;
                     }
                     if (outputImage.GetPixel(x, y).B >= 240 && outputImage.GetPixel(x, y).G >= 240 && outputImage.GetPixel(x, y).R >= 240)
                     {
-                        outWhite++;
-                    } 
+                        outputImageBlackPixels++;
+                    }
                     else if (outputImage.GetPixel(x, y).B <= 15 && outputImage.GetPixel(x, y).G <= 15 && outputImage.GetPixel(x, y).R <= 15)
                     {
-                        outBlack++;
+                        outputImageWhitePixels++;
                     }
                     if (noisedImage.GetPixel(x, y).B >= 240 && noisedImage.GetPixel(x, y).G >= 240 && noisedImage.GetPixel(x, y).R >= 240)
                     {
-                        noiseWhite++;
+                        noisedImageWhitePixels++;
                     }
                     else if (noisedImage.GetPixel(x, y).B <= 15 && noisedImage.GetPixel(x, y).G <= 15 && noisedImage.GetPixel(x, y).R <= 15)
                     {
-                        noiseBlack++;
+                        noisedImageBlackPixels++;
                     }
-                    
                 }
             }
 
             // The following doubles are the amount of white / black pixels in the input, noised image and output image
-            double inputNoise = inBlack + inWhite;          // 40
-            double noisedNoise = noiseBlack + noiseWhite;    // 80
-            double outputNoise = outBlack + outWhite;       // 20
+            double inputImageTotalNoise = inputImageBlackPixels + inputImageWhitePixels;          // 40
+            double noisedImageTotalNoise = noisedImageBlackPixels + noisedImageWhitePixels;    // 80
+            double outputImageTotalNoise = outputImageBlackPixels + outputImageWhitePixels;       // 20
 
             // The total noise is the added noise only.
-            double totalNoise = noisedNoise - inputNoise;
+            double totalNoise = noisedImageTotalNoise - inputImageTotalNoise;
 
+            Console.WriteLine(totalNoise);
             // Output percentage.
-            double percentOut = outputNoise / totalNoise * 100 - 100;
+            double percentOut = outputImageTotalNoise / totalNoise * 100 - 100;
+            Console.WriteLine(percentOut);
 
             return "Noise removed: " + Math.Abs(Math.Round(percentOut, 4)) + "%.";
         }
